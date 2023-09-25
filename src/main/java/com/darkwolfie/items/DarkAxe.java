c6b64eb39b4139a313ef6ab592291793a75e1dd7
DarkWolfie <darkwolfie@darksmp.com>
2023-09-25 02:14:39 -0400

    Axe Update

--- src/main/java/com/darkwolfie/items/DarkAxe.java
+++ src/main/java/com/darkwolfie/items/DarkAxe.java
@@ -2,6 +2,9 @@ package com.darkwolfie.items;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.world.World;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Hand;
@@ -11,11 +14,32 @@ import net.minecraft.sound.SoundEvents;
 
 
 
-public class DarkAxe extends Item {
+public class DarkAxe extends Item implements ToolMaterial {
     public DarkAxe(Settings settings){
         super(settings);
     }
-
+    public int getDurability(){
+        return 500;
+    }
+    public float getMiningSpeed(){
+        return 0.8f;
+    }
+    public float getAttackDamage(){
+        return 2.0f;
+    }
+    public int getMiningLevel(){
+        return 7;
+    }
+    public int getEnchantability(){
+        return 100;
+    }
+    public Ingredient getRepairIngredient(){
+        return Ingredient.ofItems(Items.ROTTEN_FLESH);
+    }
+    public float getMiningSpeedMultiplier(){
+        return 1.0f;
+    }
+    
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
         playerEntity.playSound(SoundEvents.BLOCK_WOOL_BREAK, SoundCategory.MASTER, 1.0f, 1.0f);
